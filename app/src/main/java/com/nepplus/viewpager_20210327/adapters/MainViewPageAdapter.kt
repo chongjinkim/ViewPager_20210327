08094c952382f8adee39de8bcea8ca256df3ecfd
김종진 <martia0118@gmail.com>
2021-03-27 14:16:08 +0900

    코틀린 문법 - return when 체험

--- app/src/main/java/com/nepplus/viewpager_20210327/adapters/MainViewPageAdapter.kt
+++ app/src/main/java/com/nepplus/viewpager_20210327/adapters/MainViewPageAdapter.kt
@@ -10,32 +10,23 @@ import com.nepplus.viewpager_20210327.fragments.PageFragment3
 class MainViewPageAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm){
 
 //position에 따른 페이지의 제목 리턴
-
+//리턴을 하는데 -> position값에 따라 다른 결과가 리턴되도록 하는 코틀린 문법 예시
     override fun getPageTitle(position: Int): CharSequence? {
-        if(position == 0){
-            return "인사문구"
-        }
-        else if(position == 1){
-            return  "자기소개"
-        }
-
-        else{
-            return "하고 싶은 말"
+        return when(position){
+            0 -> "인사"
+            1 -> "자기소개"
+            else -> "하고싶은말"
         }
     }
 
     override fun getItem(position: Int): Fragment {
 //position의 값에 따라 다른 Fragment 리턴
 
-        if(position == 0){
-            return PageFragment1()
-        }
-        else if(position == 1){
-            return PageFragment2()
-        }
+        return when(position){
+            0 -> PageFragment1()
+            1 -> PageFragment2()
+            else -> PageFragment3()
 
-        else{
-            return PageFragment3()
         }
     }
 
